30196b78d4088513ec86410bee5d8f1087f82288
Andrey Savchenko <contact@rarst.net>
2019-12-24 13:38:55 +0200

    Added namespace and dropped autoload.
    See #1

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use PHP_CodeSniffer\Util\Tokens;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/CognitiveComplexity/Analyzer.php';
 require_once __DIR__ . '/../vendor/squizlabs/php_codesniffer/autoload.php';
 
 if (! defined('PHP_CODESNIFFER_VERBOSITY')) {
