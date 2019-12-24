db82397742783a2e2e1d2d8704fc527238e07cb2
Andrey Savchenko <contact@rarst.net>
2019-12-24 16:10:00 +0200

    Added sniff test.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 use PHP_CodeSniffer\Util\Tokens;
 
-require_once __DIR__ . '/../src/CognitiveComplexity/Analyzer.php';
 require_once __DIR__ . '/../vendor/squizlabs/php_codesniffer/autoload.php';
+require_once __DIR__ . '/../src/CognitiveComplexity/Analyzer.php';
+require_once __DIR__ . '/../src/CognitiveComplexity/Sniffs/Complexity/MaximumComplexitySniff.php';
 
 if (! defined('PHP_CODESNIFFER_VERBOSITY')) {
     define('PHP_CODESNIFFER_VERBOSITY', 0);
